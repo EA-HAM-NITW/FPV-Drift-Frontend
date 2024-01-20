a3a3e55ddb5222f3872092e11e4f660107374d63
Jytesh <44925963+Jytesh@users.noreply.github.com>
2024-01-20 12:15:16 +0530

    Fix server
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { WebSocketProvider } from './context/WebSocketProvider.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { WebSocketProvider } from "./context/WebSocketProvider.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WebSocketProvider url='ws://localhost:8080'>
-    <App />
+    <WebSocketProvider url="ws://15.206.215.29:8080/">
+      <App />
     </WebSocketProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
